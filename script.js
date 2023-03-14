b69bb2a05b020a187ef0b3fbbee33d957d2a1f20
flowersayo <flowersayo0811@ewhain.net>
2023-03-14 19:45:36 +0900

    feat: addTodo

---
+++ script.js
@@ -0,0 +1,46 @@
+function addTodo() {
+  const input = document.getElementsByClassName("todo-input-box__input")[0];
+
+  if (input.value === "") {
+    //nothing happend when value is blank
+
+    return;
+  }
+  const todoLists = document.getElementsByClassName("tasks__todo__content")[0];
+
+  // 1. create list node
+
+  const newTaskNode = document.createElement("li");
+
+  const uncheckIcon = document.createElement("img");
+  uncheckIcon.src = "./images/uncheck.svg";
+  uncheckIcon.className = "icon__hover";
+  uncheckIcon.onclick = function (e) {
+    // moveToDone(e.target.parentElement);
+  };
+  newTaskNode.appendChild(uncheckIcon);
+
+  const textNode = document.createTextNode(input.value);
+  newTaskNode.appendChild(textNode);
+
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "./images/delete.svg";
+  deleteIcon.className = "icon__hover";
+
+  deleteIcon.onclick = function (e) {
+    e.target.parentElement.remove();
+  };
+  newTaskNode.appendChild(deleteIcon);
+
+  // 2. append to parent container
+
+  todoLists.appendChild(newTaskNode);
+
+  console.log(newTaskNode);
+  alert("add success");
+  input.value = ""; //input flush
+}
+
+const inputBtn = document.getElementsByClassName("todo-input-box__icon")[0];
+const input = document.getElementsByClassName("todo-input-box__input")[0];
+inputBtn.onclick = input.onsubmit = addTodo;
