6671211375ada11b3342fec337e20b041ae76f9a
권가은 <aileen1007@ewhain.net>
2023-03-17 17:37:23 +0900

    feat: display new-todo

--- todoList.js
+++ todoList.js
@@ -1,7 +1,22 @@
 const todoForm = document.getElementById('input-form');
 const todoInput = document.querySelector('#input-form input');
+const todoLists = document.getElementsByClassName('todo-lists')[0];
+const doneLists = document.getElementsByClassName('done-lists')[0];
 
 let todos = [];
+
+const displayNewTodo = (newTodo) => {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  const btn = document.createElement('button');
+  div.id = newTodo.id;
+  span.innerText = newTodo.text;
+  btn.innerText = '✔';
+
+  div.appendChild(span);
+  div.appendChild(btn);
+  todoLists.appendChild(div);
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -13,6 +28,8 @@ const handleSubmit = (e) => {
   };
   todos.push(newTodoObject);
   localStorage.setItem('todos', JSON.stringify(todos));
+
+  displayNewTodo(newTodoObject);
 
   todoInput.value = '';
 };
