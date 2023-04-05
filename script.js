c58553aab6eed7d0cdb998f803149516818e1dff
SujinRo <shtnwls1111@ewhain.net>
2023-04-05 17:25:30 +0900

    .

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const todoInput = document.querySelector('#inputPlan'),
   todoCount = document.querySelector('.todoCount'),
   doneCount = document.querySelector('.doneCount');
 
-let todoC = 0; //to do의 list 갯수
-let doneC = 0; //done의 list 갯수
-
 //enter을 누르면 input 내용이 to do 섹션으로 넘어감
 function enterkey() {
   if (window.event.keyCode == 13) {
@@ -37,11 +34,8 @@ function click_addBtn() {
   addList.className = 'addList';
 
   if (todoInput.value != '') {
-    todoC++;
     todoList.appendChild(addList);
     todoInput.value = '';
-
-    showCount();
   } else {
     alert('No item!');
   }
@@ -52,8 +46,6 @@ function moveDone(text, button) {
   var moveDoneList = document.createElement('li');
   var moveDoneText = document.createElement('text');
   var moveDoneBtn = document.createElement('button');
-  todoC--;
-  doneC++;
 
   moveDoneText = text;
   moveDoneBtn = button;
@@ -66,11 +58,8 @@ function moveDone(text, button) {
   });
 
   moveDoneList.className = 'doneList';
-
   moveDoneList.append(moveDoneText, moveDoneBtn);
   doneList.appendChild(moveDoneList);
-
-  showCount();
 }
 
 //doneList섹션에서 text를 누르면 todoList섹션으로 넘어감
@@ -78,8 +67,6 @@ function moveTodo(text, button) {
   var moveAddList = document.createElement('li');
   var moveTodoText = document.createElement('text');
   var moveTodoBtn = document.createElement('button');
-  todoC++;
-  doneC--;
 
   moveTodoText = text;
   moveTodoBtn = button;
@@ -92,32 +79,12 @@ function moveTodo(text, button) {
   });
 
   moveAddList.className = 'addList';
-
   moveAddList.append(moveTodoText, moveTodoBtn);
   todoList.appendChild(moveAddList);
-
-  showCount();
 }
 
 //text옆 ❎버튼을 누르면 내용 delete
 function click_deleteBtn(listName) {
   const list = event.target.parentElement;
   list.remove();
-
-  if (listName == todoList) {
-    todoC--;
-  } else {
-    doneC--;
-    todoC++;
-  }
-
-  showCount();
 }
-
-//count
-function showCount() {
-  todoCount.innerHTML = 'items: ' + todoC;
-  doneCount.innerHTML = 'items: ' + doneC;
-}
-
-showCount();
